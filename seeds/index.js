1c8298f017b07d49e8310fac7d4f5fd5e221b474
Glen Hood <glenhoodatx@gmail.com>
2021-08-03 21:01:30 -0500

    edited seeds mistake

--- seeds/index.js
+++ seeds/index.js
@@ -1,33 +1,20 @@
-const { users } = require('../models');
+const sequelize = require('../config/connection');
+const seedUsers = require('./userData')
+const seedPosts = require('./PostsData');
+const seedResponses = require('./responsesData');
 
-const userdata = [
-  {
-      username:"Techguy1",
-      password:"Jijefhwiueni",
-      email:"Techguy1@gmail.com",
-      image_url:""
-  },
-  {
-      username:"Techgal2",
-      password:"9wcj9iewncoiw",
-      email:"Techgal1@gmail.com"
-  },
-  {
-      username:"Wonderboy18",
-      password:"ce0j2c0cncas",
-      email:"Wonderboy18@gmail.com"
-  },
-  {
-      username:"GOTfan00",
-      password:"ndkjcnksdlah",
-      email:"GOTfan00@gmail.com"
-  },
-  {
-      username:"TexKing",
-      password:"cdsdcscmioq",
-      email:"Tex@gmail.com"
-},
-]
-const seedUsers = () => users.bulkCreate(userdata);
 
-module.exports = seedUsers;
+const seedAll = async () => {
+  await sequelize.sync({ force: true });
+
+  await seedUsers();
+
+  await seedPosts();
+
+  await seedResponses();
+
+  process.exit(0);
+};
+
+seedAll();
+
