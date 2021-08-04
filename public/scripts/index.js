f1f658e9b6a934d1a31b59f38cdf0bc6ddc82d1c
Brandon Norsworthy <brandonknorsworthy@gmail.com>
2021-08-04 16:25:14 -0500

    upvote button is not interactable

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,13 +1,33 @@
 function init() {
-    // sets active id for any clicked button and clears all other buttons of that id
     $(".sortBtn").click(sortButtonClicked)
+    $(".upvote").click(upvoteButtonClicked)
 }
 
 function sortButtonClicked() {
+    //TODO fetch sorted homepage by user selected category
+
+    // sets active id for any clicked button and clears all other buttons of that id
     for (let index = 0; index < $(".sortBtn").length; index++) {
         $(".sortBtn")[index].id = ""
     }
     $(this).attr("id","active")
+}
+
+function upvoteButtonClicked() {
+    //TODO stop propogation upwards
+
+    //? if upvote has upvote-activated class already then take it off
+    if ($(this)[0].classList.contains("upvote-activated")) {
+        $(this).removeClass("upvote-activated")
+    } else {
+        //? else put it on
+        $(this).addClass("upvote-activated")
+    }
+
+}
+
+function threadCardClicked() {
+
 }
 
 init()
