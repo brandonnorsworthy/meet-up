f1b14fa39263fac736770ed0e007c618fb37b810
Brandon Norsworthy <brandonknorsworthy@gmail.com>
2021-08-04 17:08:33 -0500

    started shifting leftover styles in html over to content.css

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,33 +1,37 @@
 function init() {
-    $(".sortBtn").click(sortButtonClicked)
-    $(".upvote").click(upvoteButtonClicked)
+    $(".sortBtn").click(sortButtonClicked);
+    $(".upvote").click(upvoteButtonClicked);
+    $(".thread").click(threadCardClicked);
 }
 
-function sortButtonClicked() {
+function sortButtonClicked(event) {
     //TODO fetch sorted homepage by user selected category
 
     // sets active id for any clicked button and clears all other buttons of that id
     for (let index = 0; index < $(".sortBtn").length; index++) {
-        $(".sortBtn")[index].id = ""
+        $(".sortBtn")[index].id = "";
     }
-    $(this).attr("id","active")
+    $(this).attr("id","active");
 }
 
-function upvoteButtonClicked() {
+function upvoteButtonClicked(event) {
     //TODO stop propogation upwards
+    event.stopPropagation();
 
     //? if upvote has upvote-activated class already then take it off
     if ($(this)[0].classList.contains("upvote-activated")) {
-        $(this).removeClass("upvote-activated")
+        $(this).removeClass("upvote-activated");
     } else {
         //? else put it on
-        $(this).addClass("upvote-activated")
+        $(this).addClass("upvote-activated");
     }
 
 }
 
-function threadCardClicked() {
-
+function threadCardClicked(event) {
+    //TODO redirect user to thread page on click using the id attatched to the card /post/:id
+    console.log("redirecting")
+    location.href = 'post.html';
 }
 
 init()
