a112464dc7a9c9d0f5f47da0bd95e77dcacffe64
Brandon Norsworthy <brandonknorsworthy@gmail.com>
2021-08-06 12:39:35 -0500

    fix double const declaration in connection.js crashed server

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,6 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-const Sequelize = require('sequelize');
-require('dotenv').config();
-
 let sequelize;
 
 if (process.env.JAWSDB_URL) {
