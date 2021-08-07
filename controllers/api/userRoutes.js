4c5f53fd25b3104b8a7b2134064d946e479b3aaf
Brandon Norsworthy <brandonknorsworthy@gmail.com>
2021-08-06 20:39:03 -0500

    cleaned up userRoutes get ready for creating an account

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -2,70 +2,93 @@ const router = require('express').Router();
 
 const { Users } = require('../../models');
 
-router.post('/login',async function(req, res){
-	//TODO attach the user id to the session so we can look at who is logged in at anytime
+router.post('/login', async function (req, res) {
 	try {
-    const dbUserData = await Users.findOne({
-      where: {
-        email: req.body.email,
-      },
-    });
+		//find a user with the given email
+		const dbUserData = await Users.findOne({ where: { email: req.body.email }});
 
-		let user = dbUserData.get({ plain: true })
+		//if there is no user with that email send back a bad request
+		if (!dbUserData) {
+			res.status(400).json({ message: 'Incorrect email or password. Please try again!' });
+			return;
+		}
 
-    if (!dbUserData) {
-      res.status(400)
-      .json({ message: 'Incorrect email or password. Please try again!' });
-      return;
-    }
+		//go to database helper function and check hashed password against stored hashed
+		const validPassword = await dbUserData.checkPassword(req.body.password);
 
-    const validPassword = await dbUserData.checkPassword(req.body.password);
+		//if the entered password was invalid then send back a bad request error
+		if (!validPassword) {
+			res.status(400).json({ message: 'Incorrect email or password. Please try again!' });
+			return;
+		}
 
-    if (!validPassword) {
-      res
-        .status(400)
-        .json({ message: 'Incorrect email or password. Please try again!' });
-      return;
-    }
+		console.log('login⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠', dbUserData.get({ plain: true }));
 
-    req.session.save(() => {
-      req.session.loggedIn = true;
-      req.session.username = user.username.toUpperCase().trim();
-      res.status(200).json({ message: 'You are now logged in!' });
-    });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json(err);
-  }
+		req.session.save(() => {
+			req.session.loggedIn = true;
+			req.session.user = dbUserData.get({ plain: true });
+		});
+
+		res.status(202);
+	} catch (err) {
+		console.log(err);
+		res.status(500).json(err);
+	}
 });
 
-router.post('/register',async function(req, res){
+router.post('/register', async function (req, res) {
 	try {
-    const dbUserData = await Users.create({
-      username: req.body.username,
-      email: req.body.email,
-      password: req.body.password,
-    });
+		//check database for a user that contains this email
+		const dbEmailData = await Users.findOne({ where: { email: req.body.email } });
 
-    req.session.save(() => {
-      req.session.loggedIn = true;
+		//if a user with that email is found then send back that email already exsists
+		if (dbEmailData) {
+			res.status(400).json({ message: 'email exists' });
+			return;
+		}
 
-      res.status(200).json(dbUserData);
-    });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json(err);
-  }
+		//check database for a user that contains this username
+		const dbUsernameData = await Users.findOne({ where: { username: req.body.username } });
+
+		//if that username is found in the database then return that username is taken
+		if (dbUsernameData) {
+			res.status(400).json({ message: 'username exists' });
+			return;
+		}
+
+		//TODO take in a url or image some how
+		//creates a new user in database that can be logged in from
+		const dbUserData = await Users.create({
+			username: req.body.username.trim(),
+			email: req.body.email.toLowerCase().trim(),
+			password: req.body.password.trim(),
+		});
+
+		console.log('register⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠⚠', dbUserData.get({ plain: true }));
+
+		//after users account is created automatically log them into that account
+		req.session.save(() => {
+			req.session.loggedIn = true;
+			req.session.user = dbUserData.get({ plain: true });
+		});
+
+		//send back 201 everything went ok
+		res.status(201);
+	} catch (err) {
+		console.log(err);
+		res.status(500).json(err);
+	}
 });
 
-router.post('/logout',function(req, res){
+router.post('/logout', function (req, res) {
+	//if there is a existing session then it will allow them to try and destroy the session
 	if (req.session.loggedIn) {
-    req.session.destroy(() => {
-      res.status(204).end();
-    });
-  } else {
-    res.status(404).end();
-  }
+		req.session.destroy(() => {
+			res.status(204).end();
+		});
+	} else {
+		res.status(404).end();
+	}
 });
 
 module.exports = router;
