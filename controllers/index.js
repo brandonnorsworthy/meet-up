c103e9a5a43d45f634ea0878b0bfe85487b520c1
Brandon Norsworthy <brandonknorsworthy@gmail.com>
2021-08-06 20:42:11 -0500

    update main route index to accomidate for delted tests folder

--- controllers/index.js
+++ controllers/index.js
@@ -1,15 +1,9 @@
 const router = require('express').Router();
 
-// const userRoutes = require('./userRoutes');
-// const publicRoutes = require('./publicRoutes');
 const htmlRoutes = require('./html');
 const apiRoutes = require('./api')
-const testRoutes = require('./test')
 
-// router.use('/', userRoutes);
-// router.use('/',publicRoutes);
 router.use('/', htmlRoutes);
 router.use('/api', apiRoutes);
-router.use('/test', testRoutes);
 
 module.exports = router;
