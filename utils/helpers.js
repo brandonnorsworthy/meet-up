ebaa0cb117b2307042bc45e3df98a46c744608e6
Brandon Norsworthy <brandonknorsworthy@gmail.com>
2021-08-10 21:58:15 -0500

    remove old assets

--- utils/helpers.js
+++
@@ -1,6 +0,0 @@
-// export const format_time = function format_time(date) {
-//   return date.toLocaleTimeString();
-// }
-// export const format_date =  function format_date(date) {
-//   return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(date).getFullYear() + 5}`;
-// }
